802fe6cd695052f1069535ceda7471bf3835ccc7
MarcoCJR <marco.camposjr@sptech.school>
2022-06-04 04:12:02 -0300

    API de cadastro/login pronta

---
+++ API/src/routes/usuarios.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.get("/", function (req, res) {
+    usuarioController.testar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+    usuarioController.listar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.entrar(req, res);
+});
+
+router.post("/cadastrarU", function (req, res) {
+    usuarioController.cadastrarU(req, res);
+})
+
+router.post("/cadastrarF", function (req, res) {
+    usuarioController.cadastrarF(req, res);
+})
+
+router.post("/autualizarFKF", function (req, res) {
+    usuarioController.autualizarFKF(req, res);
+})
+module.exports = router;
