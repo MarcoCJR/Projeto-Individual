5d96033c26c98234416cdff8abccc565af7d7808
MarcoCJR <marco.camposjr@sptech.school>
2022-06-05 20:38:09 -0300

    Finalizado o projeto. Falta o Powerpoint

--- API/public/BD/Script.sql
+++ API/public/BD/Script.sql
@@ -20,5 +20,12 @@ SELECT * FROM Usuario;
 
 INSERT INTO Voto VALUES (NULL, 'Imperio'),
 						(NULL, 'Rebeldes');
+
+INSERT INTO Usuario VALUES (NULL, 'Pessoa1', 'pessoa1@gmail.com', '11 12345-6789', '123', '1000'),
+						   (NULL, 'Pessoa2', 'pessoa2@gmail.com', '11 98273-3425', '123', '1000'),
+                           (NULL, 'Pessoa3', 'pessoa3@gmail.com', '11 92635-7263', '456', '1000'),
+                           (NULL, 'Pessoa4', 'pessoa4@gmail.com', '11 98347-3200', '789', '1000'),
+                           (NULL, 'Pessoa5', 'pessoa5@gmail.com', '11 97163-8746', '012', '1000'),
+                           (NULL, 'Pessoa6', 'pessoa6@gmail.com', '11 94022-3200', '345', '1000');
                         
 SELECT Voto.Escolha, COUNT(Usuario.idUsuario) AS 'qntd_voto' FROM Usuario JOIN Voto ON fkVoto = idVoto GROUP BY Voto.Escolha;
